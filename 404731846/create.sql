8870a04eb30c3f36e7771750de4b38c0aa2cd7ee
Akshara Sundararajan <akshara.sundar@gmail.com>
2019-01-25 13:27:47 -0800

    create.sql contains all the SQL queries creating tables using create table ...

---
+++ 404731846/create.sql
@@ -0,0 +1,11 @@
+create table Movie(id int, title varchar(100), year int, rating varchar(10), company varchar(50));
+create table Actor(id int, last varchar(20), first varchar(20), dob date, dod date);
+create table Sales(mid int, ticketsSold int, totalIncome int);
+create table Director(id int, last varchar(20), first varchar(20),dob date, dod date);
+create table MovieGenre(mid int, genre varchar(20));
+create table MovieDirector(mid int, did int);
+create table MovieActor(mid int, aid int, role varchar(50));
+create table MovieRating(mid int, imdb int, rot int);
+create table Review(name varchar(20), time timestamp, mid int, rating int, comment varchar(500));
+create table MaxPersonID(id int);
+create table MaxMovieID(id int);
