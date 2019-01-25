0ff11cc11e7d6b3b9c04126c76abe67b4fb53541
Akshara Sundararajan <akshara.sundar@gmail.com>
2019-01-25 14:46:54 -0800

    Adding testing script and beginnings of the php file

---
+++ 404731846/query.php
@@ -0,0 +1,23 @@
+/*
+PHP file using a textarea to receive a query from the user and display the results after interacting with MySQL.
+*/
+
+<html>
+<head><title>CS143 Project 1A Demo</title></head>
+<body>
+<p>
+Created by: Akshara Sundararajan and Rubia Liu
+<br>
+Enter a select query.
+<p>
+<form action="." method="GET">
+<textarea name="query" cols="60" rows="8"></textarea><br />
+<input type="submit" value="Submit" />
+</form>
+</p>
+<p><small>Note: tables and fields are case sensitive.</small>
+</p>
+
+
+</body>
+</html>
