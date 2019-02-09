c4f7be17a78839c21c4f0b95bef1b8169621ed26
starruby15 <starruby15@gmail.com>
2019-02-09 14:18:30 -0800

    Finished search page

--- 404731846/search.php
+++ 404731846/search.php
@@ -18,7 +18,16 @@ Enter a keyword:
         $input = trim(htmlspecialchars($_POST['input']));
         $keywords = preg_split("/[\s,]+/",$input);
         $arrlength = count($keywords);
-        if(isset($arrlength)) {
+        if(isset($arrlength) && ($arrlength > 0) && ($keywords[0]!="")) {
+          $query = "select * from Actor where";
+          for($x = 0; $x < $arrlength; $x++) {
+            $query=$query." concat(first,' ',last) like '%".$keywords[$x]."%'";
+            if($x !== $arrlength-1) {
+              $query=$query." and ";
+            }
+          }
+          $actors_found = mysql_query("$query");
+
           $query = "select id,title,year from Movie where ";
           for($x = 0; $x < $arrlength; $x++) {
             $query=$query."title like '%".$keywords[$x]."%'";
@@ -26,11 +35,27 @@ Enter a keyword:
               $query=$query." and ";
             }
           }
+          $movies_found = mysql_query("$query");
+
           echo "<br><hr>";
           echo "<h3>Found Actors by first/last name</h3>";
+          echo "<table border='1'>";
+          echo "<tr><th>id</th><th>name</th><th>sex</th><th>dob</th><th>dod</th></tr>";
+          while($row = mysql_fetch_row($actors_found))
+          {
+              echo "<tr>";
+              echo "<td align='center'>".$row[0]."</td>";
+              echo "<td align='center'>
+              <a href='ShowActorInfo.php?aid=".$row[0]."'>".$row[2]." ".$row[1]."</a></td>";
+              echo "<td align='center'>".$row[3]."</td>";
+              echo "<td align='center'>".$row[4]."</td>";
+              echo "<td align='center'>".$row[5]."</td>";
+  	          echo "</tr>";
+          }
+          echo "</table>";
+
           echo "<br><hr>";
           echo "<h3>Found movies by title</h3>";
-          $movies_found = mysql_query("$query");
           echo "<table border='1'>";
           echo "<tr>";
           while ($i < mysql_num_fields($movies_found))
