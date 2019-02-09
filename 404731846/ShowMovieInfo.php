cf5b2f7c9022f298ddaacbdd0cf6d2a42aaa13de
starruby15 <starruby15@gmail.com>
2019-02-09 02:53:41 -0800

    Added actors in this movie to show movie's info

--- 404731846/ShowMovieInfo.php
+++ 404731846/ShowMovieInfo.php
@@ -56,6 +56,32 @@ PHP file using a textarea to receive a query from the user and display the resul
           echo "<br>";
           echo "</p>";
 
+          echo "<br><hr>";
+
+          echo "<h3>Actors in this Movie</h3>";
+
+          $query = "select aid, role from MovieActor where mid=".$movie_id;
+          $movieactor_info = mysql_query("$query");
+          $num_rows = mysql_num_rows($movieactor_info);
+          if(is_null($num_rows)) {
+            print "<p>This movie has no listed actors.</p>";
+          }
+          else {
+            echo "<table border='1'>";
+            echo "<tr><th>name</th><th>role</th></tr>";
+            while($row = mysql_fetch_row($movieactor_info))
+            {
+                $test="select first,last from Actor where id =".$row[0];
+                $actor_name = mysql_query("$test");
+                $isolate_actor_name = mysql_fetch_row($actor_name);
+                echo "<tr>";
+                echo "<td align='center'><a href='ShowActorInfo.php?aid=".$row[0]."'>".$isolate_actor_name[0]." ".$isolate_actor_name[1]."</a></td>";
+                echo "<td align='center'>".$row[1]."</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+          }
+
         }
 
         mysql_close($db_connection);
